f5270c7c108428a2f753f31b6ea54b53a97151ba
geovannigabriel <gabriel.1087@hotmail.com>
2020-03-09 23:09:43 -0600

    Added splash screen and density drawable folders

---
+++ app/src/main/java/com/geovanni/banks/BanksApp.kt
@@ -0,0 +1,12 @@
+package com.geovanni.banks
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class BanksApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/geovanni/banks/views/activities/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.geovanni.banks.views.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.geovanni.banks.R
+
+class SplashScreenActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
